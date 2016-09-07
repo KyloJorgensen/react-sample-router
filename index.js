1eb233887ad3a99b1913ab2e92f986f7b330d00b
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-09-07 16:00:03 -0600

    added idex.html and edit index.js setting up react

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Start = function() {
+	return (
+		<div> 
+			<p>Hello Wolrd</p>
+		</div>
+	);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<Start />, document.getElementById('app'));
+});
